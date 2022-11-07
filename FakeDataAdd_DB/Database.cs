3d645352a81a57234bbba13e5f042f9913c71d57
celal <celalkilnc@gmail.com>
2022-11-07 07:34:38 +0300

    Add project files.

---
+++ FakeDataAdd_DB/Database.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace FakeDataAdd_DB
+{
+    internal class Database
+    {
+        const string connectionString = @"Data Source=LAPTOP-60OVNJGF;Initial Catalog=LibraryTest;Integrated Security=True";
+        static SqlConnection connection = new SqlConnection(connectionString);
+        static SqlCommand command;
+
+        public void AddUserDB(int id, string name, string surname, string schoolname, string email, string password)
+        {
+            command = new SqlCommand(@"INSERT INTO tblUsers(UserID,UserName,UserSurname,refSchoolID,UserEmail,UserPassword) VALUES (@id,@name,@surname,@schoolId,@email,@password)", connection);
+
+            #region commands
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@surname", surname);
+            command.Parameters.AddWithValue("@schoolId", schoolname);//Düzeltme : 'refSchoolID' int(id) değil 'okul adı'(string) alır
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@password", password);
+            #endregion
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public int idCreator()
+        {
+            Random random = new Random();
+            int id;
+            bool ctrl = false;
+            while (ctrl)
+            {
+                id = random.Next(1000000, 9999999);
+                if (!idControl(id))
+                {
+                    return id;
+                }
+            }
+            return 0;
+        }
+        private bool idControl(int id)
+        {
+            SqlCommand tblschlcommand = new SqlCommand("Select UserID From TblUsers", connection);
+            tblschlcommand.CommandType = CommandType.Text;
+            SqlDataReader reader;
+
+            connection.Open();
+            reader = tblschlcommand.ExecuteReader();
+            while (reader.Read())
+            {
+                if (id == Convert.ToInt32(reader["UserID"]))
+                {
+                    connection.Close();
+                    return true;
+                }
+            }
+            connection.Close();
+            return false;
+        }
+    }
+}
---
+++ FakeDataAdd_DB/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FakeDataAdd_DB
+{
+    public partial class Form1 : Form
+    {
+        List<string> schools = new List<string>();
+        Database db = new Database();
+        public Form1()
+        {
+            InitializeComponent();
+            schools.Add("Akdeniz Üni.");
+            schools.Add("Ege Üni.");
+            schools.Add("Pamukkale Üni.");
+            schools.Add("Dokuz Eylül Üni.");
+            schools.Add("Adnan Menderes Üni.");
+            schools.Add("Aydın Üni.");
+            schools.Add("İstanbul Teknik Üni.");
+            schools.Add("Yıldız Teknik Üni.");
+            schools.Add("Hacettepe Üni.");
+            schools.Add("Özyeğin Üni.");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 400; i++)
+            {
+                #region values
+                int id = db.idCreator();
+                string name = FakeData.NameData.GetFirstName();
+                string surname = FakeData.NameData.GetSurname();
+                string school = schools[random.Next(0, (schools.Count))];
+                string email = FakeData.NetworkData.GetEmail(name, surname);
+                string password = "123";
+                #endregion
+
+                db.AddUserDB(id, name, surname, school, email, password);
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
